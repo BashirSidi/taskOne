3aeb9f9326ec79ecdb9228faa20680701666775f
BashirSidi <bashirsalisusidi@gmail.com>
2023-09-21 01:22:10 +0100

    files strucute

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ThemeRegistry from '@/theme/ThemeRegistry'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ThemeRegistry>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Navbar />
+            {children}
+          <Footer />
+        </body>
       </ThemeRegistry>
     </html>
   )
