fc4160a30bf5a68c84b74875966d42e76170acbc
BashirSidi <bashirsalisusidi@gmail.com>
2023-09-21 17:49:37 +0100

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Box, Typography } from '@mui/material'
+"use client"
+import { Box, Container, Typography } from '@mui/material'
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+const Item = styled(Paper)(({ theme }) => ({
+  // textAlign: 'center',
+}));
 
 export default function Home() {
   return (
     <main>
-      <Box>
+      <Container maxWidth="xl">
         {/* Hero */}
         <Box>
           <Typography
@@ -24,6 +32,7 @@ export default function Home() {
               textAlign: 'center',
               color: '#000',
               mt: { xs: '16px', md: '40px' },
+              mb: { xs: '16px', md: '60px' },
               fontSize: { xs: '14px', md: '18px' },
               fontWeight: 400,
               lineHeight: '160%',
@@ -35,7 +44,43 @@ export default function Home() {
             the readable content of a page when looking at its layout.
           </Typography>
         </Box>
+
+        <Box 
+          component='img'
+          src="/images/largeImageOne.svg" 
+          alt="product image one"
+          sx={{ 
+            width: '100%',
+            height: { xs: '', md: '' },
+            display: { xs: 'flex', md: 'flex' },
+            mb: { xs: '16px', md: '60px' },
+          }}
+        />
+
+
+      {/* overview / process */}
+      <Box
+        sx={{
+          display: { xs: 'none', md: 'flex' },
+        }}
+      >
+        <Box 
+        sx={{ 
+          flexGrow: 1,
+        }}
+        >
+          <Grid container spacing={2}>
+            <Grid xs={6}>
+              <Item>xs=6</Item>
+            </Grid>
+            <Grid xs={6}>
+              <Item>xs=6</Item>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
+
+      </Container>
     </main>
   )
 }
